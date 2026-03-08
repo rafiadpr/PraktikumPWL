2c4127d0845c87d941b8b71b2611bf7df51aecdf
rafiadpr <work.rafiadrianp@gmail.com>
2026-03-08 22:01:57 +0700

    [new] : finished js3; finished making category crud;

---
+++ database/migrations/2026_03_08_145642_change_category_id_to_foreign_key_in_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Hapus kolom integer yang lama jika perlu, atau langsung modifikasi
+            $table->unsignedBigInteger('category_id')->change();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
